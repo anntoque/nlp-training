275f653eef6da5263a9cb6dc00d22fa31cda4c80
anntoque <ann.toque@gmail.com>
2019-01-25 20:24:39 +0900

    カンマとピリオドを除外し、空白区切りで単語分割

---
+++ 03_from_text_to_pi.py
@@ -0,0 +1,7 @@
+input_text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+
+input_text = input_text.replace('.',"")
+input_text = input_text.replace(',',"")
+input_text = input_text.split()
+
+print(input_text)
