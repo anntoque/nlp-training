b1e7cc15278ca6fddf98389b33a66ebd65c45d58
anntoque <ann.toque@gmail.com>
2019-01-28 23:39:29 +0900

    不要なコメントを削除

--- 04_from_text_to_elements.py
+++ 04_from_text_to_elements.py
@@ -7,10 +7,8 @@ ans_dict = {}
 
 for index, value in enumerate(input_text,start=1):
     if index in (1, 5, 6, 7, 8, 9, 15, 16, 19):
-        #print(str(index) + " :" + value[:1])
         ans_dict[value[:1]] = index
     else:
-        #print(str(index) + " :" + value[:2])
         ans_dict[value[:2]] = index
 
 print(ans_dict)
