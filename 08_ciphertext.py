6de573fa4597475f9619530ac2247a688dd7e614
anntoque <ann.toque@gmail.com>
2019-02-05 19:53:11 +0900

    コメント追加

--- 08_ciphertext.py
+++ 08_ciphertext.py
@@ -1,6 +1,7 @@
 question_text = "I am the bone of my sword. Steel is my body, and fire is my blood."
 
-
+# 半角英字のみアトバシュ暗号化を行う
+# HACK: ret += が複数回出ているのでまとめれそう
 def cipher(input_text):
     ret = ""
     for character in input_text:
