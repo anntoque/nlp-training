99e57ab26786821ab4d18dd998d5168ac78c6e01
anntoque <ann.toque@gmail.com>
2019-02-05 19:48:30 +0900

    文字列が入力されたとき、各文字を暗号化する関数を実装

---
+++ 08_ciphertext.py
@@ -0,0 +1,18 @@
+question_text = "I am the bone of my sword. Steel is my body, and fire is my blood."
+
+
+def cipher(input_text):
+    ret = ""
+    for character in input_text:
+        if character.islower():
+            ret += chr(219-ord(character))
+        else:
+            ret += character
+    return ret
+
+cipher_text = cipher(question_text)
+decryption_text = cipher(cipher_text)
+
+print(cipher_text)
+print(decryption_text)
+
