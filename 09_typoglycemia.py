96cbb1e87ec120d6c8411809825f16c9ecd834bb
anntoque <ann.toque@gmail.com>
2019-02-06 23:05:30 +0900

    タイポグリセミア処理を行なった単語を空白区切の文字列にする

--- 09_typoglycemia.py
+++ 09_typoglycemia.py
@@ -2,19 +2,18 @@ import random
 
 question_text = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
 
-# TODO: シャッフル処理は実装したので、文字列をリストで開始、空白区切りで結合する処理を実装
 # 入力した文章にタイポグリセミアを適応
-
 def typoglycemia(input_text):
-    cipher_text = "" 
-    split_text = print(question_text.split())
+    cipher_text_list = []
+    split_text = question_text.split()
     for string in split_text:
         if len(string) < 4:
-            return
+            cipher_text_list.append(string)
         else:
             first_character = string[0]
             end_character = string[-1]
-            shuffle_string = ''.join(random.sample(string[1:-1], len(string)))
+            shuffle_string = ''.join(random.sample(string[1:-1], len(string[1:-1],)))
+            cipher_text_list.append(first_character + shuffle_string + end_character)
+    return ' '.join(cipher_text_list)
 
-            return first_character + shuffle_string + end_character
-        
+print(typoglycemia(question_text))
