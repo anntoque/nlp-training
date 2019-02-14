899f8d92a6ab05967c75499f7d673985e0c1aed8
anntoque <ann.toque@gmail.com>
2019-02-15 00:02:40 +0900

    readの()の閉じ忘れと軽微な修正

--- 12_first_row_two_file.py
+++ 12_first_row_two_file.py
@@ -1,10 +1,10 @@
 with open ('./hightemp.txt','r',encoding='utf-8') as file_temp:
-    text = file_temp.read
+    text = file_temp.read()
 
 list_column1 = []
 list_column2 = []
 
-for line in text.split('\n'):
+for line in text.strip().split('\n'):
     if len(line) > 0:
         list_column1.append(line.split('\t')[0])
         list_column2.append(line.split('\t')[1])
@@ -13,4 +13,4 @@ with open ('./column1.txt','a',encoding='utf-8') as file_column1:
     print(*list_column1,sep='\n',file=file_column1)
 
 with open ('./column2.txt','a',encoding='utf-8') as file_column2:
-    print(*list_column2,sep='\n',file=file_column1)
+    print(*list_column2,sep='\n',file=file_column2)
