4c2ecdbd3d56a9c7cdff71b7f3b1d08edcb9a571
anntoque <ann.toque@gmail.com>
2019-03-04 00:15:41 +0900

    指定行数分表示する処理を実装

--- 15_last_n_row.py
+++ 15_last_n_row.py
@@ -4,7 +4,8 @@ parser = argparse.ArgumentParser(description='.txtの末尾からの指定行数
 parser.add_argument('integer', metavar='N',type=int,help='表示する行数を数字で入力')
 args = parser.parse_args()
 
-text = ''
 with open('./hightemp.txt','r',encoding='utf-8') as file_temp:
-    for line in reversed(file_temp.readlines()):
-        print(line.rstrip())
+    for index,line in enumerate(reversed(file_temp.readlines())):
+        print(line.strip())
+        if index+1 == args.integer:
+            break
