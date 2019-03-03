e6a4afb18a7dcd5a92cdeb0b778d761a6e192f92
anntoque <ann.toque@gmail.com>
2019-03-03 23:54:48 +0900

    逆順で全ての行を表示

--- 15_last_n_row.py
+++ 15_last_n_row.py
@@ -3,3 +3,8 @@ import argparse
 parser = argparse.ArgumentParser(description='.txtの末尾からの指定行数分を出力する')
 parser.add_argument('integer', metavar='N',type=int,help='表示する行数を数字で入力')
 args = parser.parse_args()
+
+text = ''
+with open('./hightemp.txt','r',encoding='utf-8') as file_temp:
+    for line in reversed(file_temp.readlines()):
+        print(line.rstrip())
